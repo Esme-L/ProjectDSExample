fb18e1a11eca46ccf1c03121cf57d59d611f42d0
Esme-L <ezacariasp1600@alumno.ipn.mx>
2023-07-24 15:01:01 -0600

    datareaderpoo

--- src/models/train.py
+++ src/models/train.py
@@ -3,9 +3,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import preprocessing
+import helpers.DataLoader as Dtl
 
 # Leer los datos
-df = pd.read_csv("./data/processed/RH_procesado.csv")
+reader = Dtl.DataLoader("./data/processed/RH_procesado.csv")
+df = reader.load_data()
 
 # Convertir columnas no numéricas a numéricas
 for column in df.columns:
